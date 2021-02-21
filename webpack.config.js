d3c0343889db8424d4061c3afcccd2be20e5cba5
Sergei Iurevich <lask2su@gmail.com>
2021-02-21 19:10:42 +0300

    Setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+}
 };
