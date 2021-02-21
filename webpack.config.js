0c9c59e892d15ff4f862c2f000327476741af043
Sergei Iurevich <lask2su@gmail.com>
2021-02-21 20:27:08 +0300

    Setup devserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
-  module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }
-  ]
-}
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  }
 };
