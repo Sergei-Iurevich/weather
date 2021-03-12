784cc51eb2ccf0415c12ab88f1135442a8f353f5
Sergei Iurevich <lask2su@gmail.com>
2021-03-12 14:13:21 +0300

    Setup eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
+    "import/prefer-default-export": "off",
+    "import/extensions": [0, { js: "always" }],
+    "max-len": ["error", { ignoreComments: true }],
+    "no-console": "off",
+    "no-alert": "off",
+    "no-plusplus": "off",
+    "no-param-reassign": "off",
+    "prefer-arrow-callback": "off",
+    "func-names": "off",
   },
+  plugins: ["jest"],
 };
